c0619c8aa82b6637f888c574224763c3f744e120
Michael <michaelgquinn@gmail.com>
2025-10-05 06:24:39 +0100

    hello update

---
+++ spyder_test.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Oct  5 06:19:43 2025
+
+@author: Admin
+"""
+
+# Test file
+print("=" * 40)
+print("Testing if Spyder works!")
+print("If you see this, it's running!")
+print("=" * 40)
+
+x = 10
+y = 20
+print(f"x + y = {x + y}")
+
+git status
+git add .
+git commit -m "Add spyder_test.py"
+git push origin main
