6657ecaabdcb19d6d99b2b4b808712b0c6e93337
Michael <michaelgquinn@gmail.com>
2025-10-05 06:34:52 +0100

    CT5148_Exercises.py

--- CT5148_Exercises.py
+++ CT5148_Exercises.py
@@ -5,3 +5,8 @@ Created on Sun Oct  5 06:30:47 2025
 @author: Admin
 """
 
+names = ["john", "paul", "george", "ringo"]
+
+for i, x in enumerate(names):
+    print(i, x)
+    
--- spyder_test.py
+++ spyder_test.py
@@ -14,8 +14,3 @@ print("=" * 40)
 x = 10
 y = 20
 print(f"x + y = {x + y}")
-
-git status
-git add .
-git commit -m "Add spyder_test.py"
-git push origin main
