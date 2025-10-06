754125fe5940ac44d828ce530edb1b052b258956
Michael <michaelgquinn@gmail.com>
2025-10-06 10:09:01 +0100

    CT5148_Exercises.py

--- CT5148_Exercises.py
+++ CT5148_Exercises.py
@@ -10,3 +10,18 @@ names = ["john", "paul", "george", "ringo"]
 for i, x in enumerate(names):
     print(i, x)
     
+n = ["john", "paul", "george", "ringo"]
+n1 = ["lennon", "mccartney", "harrison", "starr"]
+instr = ["guitar", "bass", "guitar", "drums"]
+
+for (x, y, z) in zip(n, n1, instr):
+    print(x, y, z)
+
+for i, x in enumerate(zip(n, n1, instr)):
+    print(i, x)
+
+for i, (a, b, instr) in enumerate(zip(n, n1, instr)):
+    print("%d: %s %s, %s" % (i, a, b, instr))
+
+    
+    
